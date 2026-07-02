f77c2d6035247f765f2af9d85cd69a34ee987bbf
Deepika2575 <deepika2507.b@gmail.com>
2026-07-02 14:15:18 +0530

    Initial commit

---
+++ WEEK-2/Spring Core and Maven/Exercise 1 Configuring a Basic Spring Application/BookManagement/src/main/java/com/library/BookRepository.java
@@ -0,0 +1,8 @@
+package com.library;
+
+public class BookRepository {
+
+    public void displayBooks() {
+        System.out.println("Displaying books from Book Repository...");
+    }
+}
---
+++ WEEK-2/Spring Core and Maven/Exercise 4 Creating and Configuring a Maven Project/BookManagement/src/main/java/com/library/BookManagementApplication.java
@@ -0,0 +1,17 @@
+package com.library;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BookManagementApplication {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookService service = context.getBean("bookService", BookService.class);
+
+        service.showBooks();
+    }
+}
